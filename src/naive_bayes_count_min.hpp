de16faec721ecf1b114828976d6ec7ec6ad12719
Ruben Lauwaert <rubenlauwaert1999@gmail.com>
2022-12-07 10:44:02 +0100

    Update naive_bayes_count_min.hpp

--- src/naive_bayes_count_min.hpp
+++ src/naive_bayes_count_min.hpp
@@ -12,15 +12,39 @@ namespace bdap {
 class NaiveBayesCountMin : public BaseClf<NaiveBayesCountMin> {
 
 private:
+
+    int num_hashes_;
     std::vector<int> hashSeeds_;
     int amt_buckets_;
     std::map<int,std::vector<int>> bucketMatrixSpam_;
     std::map<int,std::vector<int>> bucketMatrixHam_;
 
 
+    void initHashSeeds(){
+        // Generate a random 16-bit number
+        for(int i ; i < num_hashes_ ; i++){
+            int rand16 = rand() % (65536 + 1);
+            hashSeeds_.push_back(rand16);
+        }
+    }
+
+
+    void initBucketMatrix(std::map<int,std::vector<int>> bucketMatrix){
+
+    }
+
+
 public:
     // TODO initialize your fields in the constructor
-    NaiveBayesCountMin(int num_hashes, int log_num_buckets, double threshold) {}
+    NaiveBayesCountMin(int num_hashes, int log_num_buckets, double threshold) {
+        // Set basic arguments
+        this.
+
+        // Generate num_hashes amount of different hash seeds
+
+        // Initialize bucket Matrices
+
+    }
 
     void update_(const Email &email)
     {
